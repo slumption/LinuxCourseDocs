4a653c551822def22448bea1e615438a9790b9fe
slumption <slumption@gmail.com>
2018-10-16 14:57:10 +0100

    Scripted auto commit from: dhcp-10-248-108-219.eduroam.wireless.private.cam.ac.uk

---
+++ IntroToCommandLine-ps459-Oct-2018/Linux Intro/scripts/example-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# My first shell script!
+echo "hello world"
+# Print the current directory
+echo "This script is in" $(pwd)\ "and running on" $(hostname)
+# Print the date and time
+echo "Today is:" $(date)
+
